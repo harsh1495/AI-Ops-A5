c1d7c092a9b3d2a20e3dd5ab377f9b3430592561
Harsh Mehta <hsmehta1411@gmail.com>
2020-02-28 23:32:52 -0500

    Added files

---
+++ tweet.py
@@ -0,0 +1,134 @@
+"""
+Assignmnet 3 - Group 2 - Check unit test for Travis
+"""
+import re
+import os
+from zipfile import ZipFile
+from tokenizer_fun import TweetTokenizer
+
+class TextPreprocessing:
+    """
+    This class takes a tweet as an input and returns padded word embeddings
+    """
+
+    def __init__(self, max_length_tweet=100, max_length_dictionary=2000000):
+
+        """
+        Initialize class
+        """
+        self.max_length_tweet = max_length_tweet
+        self.max_length_dictionary = max_length_dictionary
+
+        # import dictionary
+        self.embeddings = []
+        self.stopwords = []
+        i = 0
+
+        with open('dictionary.txt', 'r') as file:
+            for line in file:
+                word = line.strip()
+                self.embeddings.append(word)
+                i += 1
+                if i >= max_length_dictionary:
+                    break
+
+        with open('english_stopwords.txt', 'r') as file:
+            for line in file:
+                word = line.strip()
+                self.stopwords.append(word)
+
+        # define tokenizer
+        self.tokenizer = TweetTokenizer()
+
+    def clean_text(self, tweet):
+
+        """
+        Clean text
+        """
+
+        # lower case
+        tweet = tweet.lower()
+
+        # remove links
+        tweet = re.sub(r"(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)", '', tweet)
+
+        # replace # to ''
+        tweet = re.sub(r"#", '', tweet)
+
+        # remove numbers
+        tweet = re.sub(r"[0-9]+", '', tweet)
+
+        # remove stopwords and twitter handles
+        tweet_cleaned_list = []
+        for tweet_word in tweet.split(" "):
+            if (tweet_word not in self.stopwords) & (~tweet_word.startswith('@')):
+                tweet_cleaned_list.append(tweet_word)
+
+        # join words
+        tweet_cleaned = " ".join(tweet_cleaned_list)
+
+        return tweet_cleaned
+
+    def tokenize_text(self, tweet_cleaned):
+
+        """
+        Tokenize text
+        """
+
+        # tokenize
+        tokenized_words = self.tokenizer.tokenize(tweet_cleaned)
+
+        return tokenized_words
+
+    def replace_token_with_index(self, tokenized):
+
+        """
+        Replace token with embeddings
+        """
+
+        word_embeddings = []
+        for token in tokenized:
+            try:
+                embedding = self.embeddings.index(token)
+                word_embeddings.append(embedding)
+            except ValueError:
+                embedding = self.embeddings.index('<unknown>')
+                word_embeddings.append(embedding)
+
+        return word_embeddings
+
+    def pad_sequence(self, word_embeddings):
+
+        """
+        Pad embeddings for model
+        """
+
+        # if word_embeddings > max_length_tweet
+        if len(word_embeddings) > self.max_length_tweet:
+            word_embeddings_pad = word_embeddings[:self.max_length_tweet]
+
+        # if word_embeddings == max_length_tweet
+        elif len(word_embeddings) == self.max_length_tweet:
+            word_embeddings_pad = word_embeddings
+
+        # if word_embeddings < max_length_tweet
+        else:
+            diff = self.max_length_tweet - len(word_embeddings)
+            pad = [self.embeddings.index('<pad>')]
+            word_embeddings.extend(pad * diff)
+            word_embeddings_pad = word_embeddings
+
+        return word_embeddings_pad
+
+    def process_tweet(self, tweet):
+
+        """
+        Run all functions defined above
+        """
+
+        clean = self.clean_text(tweet)
+        tokenized = self.tokenize_text(clean)
+        word_embeddings = self.replace_token_with_index(tokenized)
+        word_embeddings_pad = self.pad_sequence(word_embeddings)
+
+        return word_embeddings_pad
